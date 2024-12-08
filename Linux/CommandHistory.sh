e1f6f81098a39f295379a92c633a6e5ecdb95d2c
=sudhanshuvlog1 <pandeysudhanshuup@gmail.com>
2024-12-08 10:46:19 +0530

    AWS & Linux Resources from Day1

---
+++ Linux/CommandHistory.sh
@@ -0,0 +1,23 @@
+    1  whoami
+    2  yum install httpd 
+    3  cd /var/www/html
+    4  pwd
+    5  cat > index.html
+    6  ls
+    7  cat index.html 
+    8  systemctl start httpd
+    9  cat index.html 
+   10  cat > index.html 
+   11  ls
+   12  mkdir scripts
+   13  ls
+   14  cd style
+   15  mkdir style
+   16  ls
+   17  pwd
+   18  mkdir assets
+   19  ls
+   20  cd /
+   21  pwd
+   22  systemctl stop httpd
+   23  history
